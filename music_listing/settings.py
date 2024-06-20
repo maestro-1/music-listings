74bfeea24dad14ea598c6779759e0240e83e7dd8
Oghenemarho Onothoja <maestroinc@Oghenemarhos-MacBook-Pro.local>
2024-06-20 10:16:29 +0200

    move file to outer folder to fix deployment

--- music_listing/settings.py
+++ music_listing/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = "music_listing.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": str(os.getenv("DB_NAME")),
-        "USER": str(os.getenv("DB_USER")),
-        "PASSWORD": str(os.getenv("DB_PASSWORD")),
-        "HOST": str(os.getenv("DB_HOST")),
-        "PORT": str(os.getenv("DB_PORT")),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
